c69ab01ed01542e7b37b548c3ec63aad7677d03f
SamPaul Isaac <sampaulisaac18@outlook.com>
2025-08-03 00:55:46 +0530

    @SamPaulIsaac Added a basic app involving actuator and swagger.

---
+++ basic-actuator-and-swagger/src/main/java/com/sam/basicActuatorAndSwagger/BasicActuatorAndSwaggerApplication.java
@@ -0,0 +1,14 @@
+package com.sam.basicActuatorAndSwagger;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class BasicActuatorAndSwaggerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BasicActuatorAndSwaggerApplication.class, args);
+    }
+}
+
