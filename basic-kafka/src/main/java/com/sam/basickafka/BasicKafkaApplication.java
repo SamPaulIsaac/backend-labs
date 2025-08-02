3d7c64dc8068699d9b632e2e3a2d8276ec87a8d2
SamPaul Isaac <sampaulisaac18@outlook.com>
2025-08-03 02:41:37 +0530

    @SamPaulIsaac Refactored code.

---
+++ basic-kafka/src/main/java/com/sam/basickafka/BasicKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.sam.basickafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicKafkaApplication.class, args);
+    }
+
+}
---
+++ basic-kafka/src/test/java/com/sam/basickafka/BasicKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.basickafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
